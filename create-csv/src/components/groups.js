df3070c7cda7f7c4bbaff6d39ba590d6de22e35f
Caio Lopes <caiolopesfv@gmail.com>
2024-04-08 09:58:09 -0300

    iniciando feature de grupos

--- create-csv/src/components/groups.js
+++ create-csv/src/components/groups.js
@@ -2,38 +2,40 @@
 import { useState, useEffect } from "react";
 import exportFromJSON from 'export-from-json'
 import ZAFClient from 'zendesk_app_framework_sdk';
-const client = ZAFClient.init(); 
+const client = ZAFClient.init();
 
-function Groups ({backToInitial}) {
+function Groups({ backToInitial }) {
 
     const [name, setName] = useState('')
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
+
+
 
     return <div className="content">
 
         <form className="form">
 
-        <button className="back" onClick={(e) => {
-            e.preventDefault()
-            backToInitial()
-        }}>— voltar</button>
+            <button className="back" onClick={(e) => {
+                e.preventDefault()
+                backToInitial()
+            }}>— voltar</button>
 
-        <label>
-            Nome do arquivo:<br />
-            <input type="text" 
-            placeholder="Insira o nome do arquivo:"
-            onChange={(e) => {setName(e.target.value)}}
-            value={name}
-            required/>
-        </label><br />
+            <label>
+                Nome do arquivo:<br />
+                <input type="text"
+                    placeholder="Insira o nome do arquivo:"
+                    onChange={(e) => { setName(e.target.value) }}
+                    value={name}
+                    required />
+            </label><br />
 
-        <button className="btn btn-macro">Gerar Planilha</button>
-        {loading && <p className="loading">carregando...</p>}
+            <button className="btn btn-macro">Gerar Planilha</button>
+            {loading && <p className="loading">carregando...</p>}
 
         </form>
 
     </div>
 
-}; 
+};
 
 export default Groups;
